f5087ef72ff94e41fdf1922635b026e52681e72e
ravatsindhav <sindhavravat999@gmail.com>
2020-02-17 23:25:41 +0530

    dynamic field

--- database/justlook.sql
+++ database/justlook.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 05, 2020 at 07:45 PM
+-- Generation Time: Feb 12, 2020 at 07:09 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -30,8 +30,8 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
-  `unm` varchar(20) NOT NULL,
-  `psw` varchar(20) NOT NULL,
+  `userid` varchar(20) NOT NULL,
+  `password` varchar(18) NOT NULL,
   `email` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -39,8 +39,23 @@ CREATE TABLE `admin` (
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`id`, `unm`, `psw`, `email`) VALUES
-(1, 'ravat', '1999', 'rs@b.com');
+INSERT INTO `admin` (`id`, `userid`, `password`, `email`) VALUES
+(1, 'ravat', '17899', 'king789@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bill`
+--
+
+CREATE TABLE `bill` (
+  `id` int(11) NOT NULL,
+  `billno` int(11) NOT NULL,
+  `customername` varchar(50) NOT NULL,
+  `mobileno` int(10) NOT NULL,
+  `total_amount` float NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -59,8 +74,57 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`id`, `category_name`, `category_detail`) VALUES
-(1, 'tre', 'wer'),
-(2, 'vb', 'vb');
+(1, '0', 'wer'),
+(2, '0', 'vb'),
+(3, '0', 'gfhgfh'),
+(4, '0', 'hjj'),
+(5, '0', 'fgfffff'),
+(6, '0', 'df'),
+(7, '0', 'df'),
+(8, '0', 'df'),
+(9, '0', 'dsf'),
+(10, '0', 'fdgdf'),
+(11, '0', 'dsf'),
+(12, '0', 'hgfhh'),
+(13, '0', 'hgfhh'),
+(14, '0', 'hgfhh'),
+(15, '0', 'hgfhh'),
+(16, '0', 'null'),
+(17, '0', 'null');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `pid` varchar(30) NOT NULL,
+  `category` varchar(30) NOT NULL,
+  `ptitle` varchar(50) NOT NULL,
+  `pprice` float NOT NULL,
+  `pqty` int(11) NOT NULL,
+  `pdetail` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `id` int(11) NOT NULL,
+  `billno` int(11) NOT NULL,
+  `customername` varchar(50) NOT NULL,
+  `mobileno` int(10) NOT NULL,
+  `productid` varchar(20) NOT NULL,
+  `pprice` float NOT NULL,
+  `pqty` int(20) NOT NULL,
+  `total` float NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -73,9 +137,27 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `bill`
+--
+ALTER TABLE `bill`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `category`
 --
 ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -89,10 +171,28 @@ ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `bill`
+--
+ALTER TABLE `bill`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
